870f4fc8422ffc273c6f0506624a6b21f116e015
Giuseppe Crupi <crupig@ion.si.usi.ch>
2026-07-09 15:40:41 +0200

    removed run eval seq

--- run_eval_sequential.sh
+++
@@ -1,7 +0,0 @@
-BASE_GENERATIONS_PATH=$1   # e.g. ./generations/google--gemma-3-1b-it/google--gemma-3-1b-it-Evalplus-part
-
-for i in {0..9}; do
-    # Replace trailing _<digit>.jsonl with _i.jsonl
-    GENERATIONS_PATH="${BASE_GENERATIONS_PATH}${i}.jsonl"
-    bash run_eval.sh $GENERATIONS_PATH
-done
